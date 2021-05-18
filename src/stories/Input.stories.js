0b2fc58c6586c7e0cb62fd1436b8b2fb052998c5
Abdul Rahiman Basith <abdulrahimanbasith@gmail.com>
2021-05-18 14:19:25 +0530

    sb

--- src/stories/Input.stories.js
+++ src/stories/Input.stories.js
@@ -10,7 +10,23 @@ export default {
   },
 };
 
-const Template = (args) => <Input {...args} />;
+const Template = (args) =>  (
+ <div>
+   <div>
+    <span style={{paddingLeft: '20px', fontSize: '16px'}}>Number Input</span>
+    <Input
+    type="number"
+    help_text="Input can only be numbers" placeholder="Enter Number"/>
+ </div>
+
+ <div>
+    <span style={{paddingLeft: '20px', fontSize: '16px'}}>Email Input</span>
+    <Input
+    type="email"
+    help_text="Input can only be an email" placeholder="Enter Email"/>
+ </div>
+ </div>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Page } from './Page';
-// import * as HeaderStories from './Header.stories';
+import * as HeaderStories from './Header.stories';
 
 export default {
   title: 'Example/Page',
@@ -10,12 +10,12 @@ export default {
 
 const Template = (args) => <Page {...args} />;
 
-// export const LoggedIn = Template.bind({});
-// LoggedIn.args = {
-//   ...HeaderStories.LoggedIn.args,
-// };
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  ...HeaderStories.LoggedIn.args,
+};
 
-// export const LoggedOut = Template.bind({});
-// LoggedOut.args = {
-//   ...HeaderStories.LoggedOut.args,
-// };
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
